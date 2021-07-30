b5f3008c4b583ac3333e08c4533387acf9ef3065
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-07-30 20:29:48 +0800

    创建Icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
-      <Icon name="money"/>
-      记账</router-link>
-    |
-    <router-link to="/labels">
+  <nav>
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="labels"/>标签
     </router-link>
-    |
-    <router-link to="/statistics">
+    <router-link to="/money" class="item" active-class="selected">
+      <Icon name="money"/>
+      记账</router-link>
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>统计
     </router-link>
-  </div>
+
+  </nav>
 </template>
 
 <script lang="ts">
@@ -22,5 +21,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+nav{
+  display: flex;
+  flex-direction: row;
+  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  .item{
+    padding: 8px 0 4px;
+    width: 33.33333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    font-size: 12px;
+    svg {
+      width: 32px; height: 32px;
+    }
+    p {
+      margin: 4px 0;
+    }
+    &.selected{
+      color: #42b983;
+    }
+  }
+}
 
 </style>
