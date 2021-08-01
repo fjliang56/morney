8fd287f799605f1e63e2caada29b8f32836fcd44
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-01 19:09:24 +0800

    使用全局tagList

--- src/costom.d.ts
+++ src/costom.d.ts
@@ -6,3 +6,20 @@ type RecordItem = {
     createdAt?: Date
 }
 
+type Tag = {
+    id: string;
+    name: string;
+}
+type TagListModel = {
+    data: Tag[]
+    fetch: () => Tag[]
+    create: (name: string) => 'success'|'duplicated'
+    update: (id: string, name: string) => 'success' | 'not found' | 'duplicated'
+    remove: (id: string) => boolean;
+    save: () => void
+}
+
+interface Window {
+    tagList: Tag[]
+}
+
